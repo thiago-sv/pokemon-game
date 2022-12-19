61bd16a822a739a031cb5b0bda3751ca9f297dd7
thiago-sv <floriponario@gmail.com>
2022-12-19 01:42:29 -0300

    "Commit inicial"

--- src/Battle.js
+++ src/Battle.js
@@ -5,8 +5,10 @@ import { Link } from 'react-router-dom';
 const Battle = () => {
     const [pokemon, setPokemon] = useState(null);
     const [pokemon2, setPokemon2] = useState(null);
+
     const [pokemonSelecionado, setPokemonSelecionado] = useState(null);
     const [pokemonSelecionado2, setPokemonSelecionado2] = useState(null);
+
 
     useEffect(() => {
         fetch('https://pokeapi.co/api/v2/pokemon/1')
@@ -38,6 +40,16 @@ const Battle = () => {
                         <img src={pokemon.sprites.front_default} alt={pokemon.name} />
                         <p>{pokemon.name}</p>
                         <p>HP: {pokemonSelecionado}</p>
+                        <p>Ataque: {pokemon.stats[1].base_stat}</p>
+                        <p>Defesa: {pokemon.stats[2].base_stat}</p>
+                        <p>Ataque Especial: {pokemon.stats[3].base_stat}</p>
+                        <p>Defesa Especial: {pokemon.stats[4].base_stat}</p>
+                        <p>Velocidade: {pokemon.stats[5].base_stat}</p>
+                        <p>Experiência: {pokemon.base_experience}</p>
+                        <p>Altura: {pokemon.height}</p>
+                        <p>Peso: {pokemon.weight}</p>
+                        <p>Tipo: {pokemon.types[0].type.name}</p>
+                        <p>Habilidade: {pokemon.abilities[0].ability.name}</p>
                         <button onClick={() => setPokemonSelecionado(pokemon.stats[0].base_stat)}>SELECIONAR</button>
                     </div>
                 )}
@@ -49,6 +61,17 @@ const Battle = () => {
                         <img src={pokemon2.sprites.front_default} alt={pokemon2.name} />
                         <p>{pokemon2.name}</p>
                         <p>HP: {pokemonSelecionado2}</p>
+
+                        <p>Ataque: {pokemon2.stats[1].base_stat}</p>
+                        <p>Defesa: {pokemon2.stats[2].base_stat}</p>
+                        <p>Ataque Especial: {pokemon2.stats[3].base_stat}</p>
+                        <p>Defesa Especial: {pokemon2.stats[4].base_stat}</p>
+                        <p>Velocidade: {pokemon2.stats[5].base_stat}</p>
+                        <p>Experiência: {pokemon2.base_experience}</p>
+                        <p>Altura: {pokemon2.height}</p>
+                        <p>Peso: {pokemon2.weight}</p>
+                        <p>Tipo: {pokemon2.types[0].type.name}</p>
+                        <p>Habilidade: {pokemon2.abilities[0].ability.name}</p>
                         <button onClick={() => setPokemonSelecionado2(pokemon2.stats[0].base_stat)}>SELECIONAR</button>
                     </div>
                 )}
